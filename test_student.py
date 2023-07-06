c2dfbe6255cb7ebaa088adeac271453e4b1ccef7
mristimaki <rosanna.ristimaki@gmail.com>
2023-07-06 00:06:26 +0000

    Add test and method to extend students end date

--- test_student.py
+++ test_student.py
@@ -1,5 +1,6 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 
 class TestStudent(unittest.TestCase):
@@ -24,6 +25,12 @@ class TestStudent(unittest.TestCase):
         self.student.alert_santa()
 
         self.assertTrue(self.student.naughty_list)
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
 
 
 if __name__ == '__main__':
